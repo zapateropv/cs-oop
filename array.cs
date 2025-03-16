6199e979c78e78b0ffa61fb768ea9a9802c3b184
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2025-03-16 14:26:11 +0800

    Create array.cs

---
+++ array.cs
@@ -0,0 +1,12 @@
+using System;
+class HelloWorld {
+  static void Main() {
+  
+  String[] cars = {"Mustang","Corvette","BMW"};
+  
+  for(int i =0; i < cars.Length; i++){
+      Console.WriteLine(cars[i]);
+  }
+  
+  }
+}
