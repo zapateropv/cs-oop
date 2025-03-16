b483ca7554ce5e36a55e1f7a73eadeee25106d54
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2025-03-16 14:29:56 +0800

    Create foreach loop.cs

---
+++ foreach loop.cs
@@ -0,0 +1,12 @@
+using System;
+class HelloWorld {
+  static void Main() {
+  
+  String[] cars = {"Mustang","Corvette","BMW"};
+  
+    foreach(String car in cars){
+        Console.WriteLine(car);
+    }
+    
+  }
+}
