b1391a5d28b11f4e6378e42fbbb047259127a21e
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2025-03-16 14:28:09 +0800

    Update array.cs

--- array.cs
+++ array.cs
@@ -1,7 +1,8 @@
 using System;
 class HelloWorld {
   static void Main() {
-  
+
+    //String[] cars = new String [4];
   String[] cars = {"Mustang","Corvette","BMW"};
   
   for(int i =0; i < cars.Length; i++){
